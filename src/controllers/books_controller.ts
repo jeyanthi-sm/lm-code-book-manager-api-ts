9a469085e5b55ad37faa744bd7045d90d886645a
Meenashi <jeyanthi_sm@yahoo.com>
2023-03-05 16:20:00 +0000

    Included the extension part simple solution

--- src/controllers/books_controller.ts
+++ src/controllers/books_controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { UniqueConstraintError } from "sequelize";
 import * as bookService from "../services/books";
 
 export const getBooks = async (req: Request, res: Response) => {
@@ -9,21 +10,22 @@ export const getBooks = async (req: Request, res: Response) => {
 export const getBook = async (req: Request, res: Response) => {
 	const bookId = req.params.bookId;
 	const book = await bookService.getBook(Number(bookId));
-
+   
 	if (book) {
 		res.json(book).status(200);
 	} else {
-		res.status(404).json("Not found");
-	}
+		res.status(404).json({message: `No Record for this id ${bookId}`});
+		}
 };
 
 export const saveBook = async (req: Request, res: Response) => {
 	const bookToBeSaved = req.body;
 	try {
 		const book = await bookService.saveBook(bookToBeSaved);
-		res.status(201).json(book);
+		res.status(201).json(book) ; 
 	} catch (error) {
-		res.status(400).json({ message: (error as Error).message });
+		  (UniqueConstraintError) ? res.status(401).json({ message: "Duplicate Record Found" })
+		: res.status(400).json({ message: (error as Error).message });
 	}
 };
 
